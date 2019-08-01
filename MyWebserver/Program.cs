e15fab9bfcf4852b59fdb1e047c966a953798a7c
spanda <spanda@tavisca.in>
2019-08-01 14:56:41 +0530

    Add project files.

---
+++ MyWebserver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyWebserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] prefixes = new String[] { "http://localhost:8080/test/" };
+            WebServer webserver = new WebServer(prefixes);
+            Console.WriteLine("Server is listening");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ MyWebserver/WebServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace MyWebserver
+{
+    public class WebServer
+    {
+
+        public WebServer(string[] prefixes)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+            
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+            HttpListener Listener = new HttpListener();
+            foreach (string s in prefixes)
+            {
+                Listener.Prefixes.Add(s);
+            }
+            Listener.Start();
+            while (Listener.IsListening)
+            {
+                Console.WriteLine("Server is listening");
+                HttpListenerContext context = Listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            
+            Listener.Stop();
+
+        }
+    }
+}
